9e9077c6c15742e8c4e774b9dec5478adbf37634
François <francois.vandenpla@decathlon.com>
2024-01-12 17:37:23 +0100

    wip

--- getarray.sh
+++ getarray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-folders=($(git show --name-only --oneline | grep folder | cut -d/ -f1 | uniq))
-
-echo "[\"${folders[*]}\"]"
+folders=( $(git show --name-only --oneline | grep folder | cut -d/ -f1 | uniq) )
+formatted_folders=$(printf '"%s", ' "${folders[@]}" | sed 's/, $//')
+echo "[$formatted_folders]"
